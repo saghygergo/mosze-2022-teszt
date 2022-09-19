4ac8d55e28db2d3a7bcbd2b172e9c0297bb62948
Sághy Gergő <saghy.geri@gmail.com>
2022-09-19 19:25:11 +0200

    Hibajavítás

--- main.cpp
+++ main.cpp
@@ -5,29 +5,29 @@ constexpr int N_ELEMENTS = 100;
 int main()
 { 
   // Hiányzik az alulvonás
-    int *b = new int[NELEMENTS];
+    int *b = new int[N_ELEMENTS];
   // Nincs pontosvessző a sor végén, rossz aposztróf használat
-    std::cout << '1-100 ertekek duplazasa'
+    std::cout << '1-100 ertekek duplazasa' << std::endl;
   // Hiányzik a léptetés
-    for (int i = 0;)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
   // 0-99 helyett 1-100ig kellene dupláznia
-        b[i] = i * 2;
+        b[i] = (i + 1) * 2;
     }
   // Hibás a ciklusvezérlő, mindig hamisba fut így nem lép be a ciklusba
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
   // Az értéket nem írja ki, és hiányzik a pontosvessző a sor végéről
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
   // A változónak nincs kezdőértéke
-    int atlag;
+    int atlag = 0;
   // Hiányzik a pontosvessző
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
   // Hiányzik a pontosvessző
-        atlag += b[i]
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
